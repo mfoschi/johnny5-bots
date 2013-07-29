74c1272573954633371596b3a2a4f56af1077675
mfoschi <tech@foschi.org>
2013-07-29 13:58:54 -0400

    Update 01-blink.js
    add a comment

--- 01-blink.js
+++ 01-blink.js
@@ -1,6 +1,12 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
+/*
+ This is the most basic, just to prove we can connect to firmata.
+ BOARD:
+ 1 LED connected at output pin #10
+ */
+
 board.on("ready", function() {
     led = new five.Led(10);
     led.strobe();
