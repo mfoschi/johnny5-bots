ab0e412fa0ff32047b8cc33afa0f1072de2ac758
mfoschi <tech@foschi.org>
2013-07-29 14:01:28 -0400

    Add basic comment

--- 02-double_blink.js
+++ 02-double_blink.js
@@ -1,6 +1,13 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
+/*
+ Execute something in a loop every 500 ms.
+ Alternate between switching one LED and the other.
+ BOARD:
+ 2 LEDs at pin 7 and 12
+ */
+
 board.on("ready", function() {
     var led1 = new five.Led({pin:7});
     var led2 = new five.Led({pin:12});
